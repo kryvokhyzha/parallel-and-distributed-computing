034c93f1287a6d41a8e088e47d917e367369a3e7
kryvokhyzha <kriwohizha@gmail.com>
2021-03-15 20:55:39 +0200

    Improve performance of non-blocking method

--- Assignment6/src/task2/main.cpp
+++ Assignment6/src/task2/main.cpp
@@ -51,11 +51,13 @@ int main(int argc, char *argv[]) {
     extra = NRA % numworkers;
     offset = 0;
 
-    MPI_Status send_status[numworkers * 4], recv_status[numworkers * 3];
-    MPI_Request send_req[numworkers * 4], recv_req[numworkers * 3];
+    MPI_Status recv_status1[numworkers * 2], recv_status2[numworkers];
+    MPI_Request send_req[numworkers * 4], recv_req1[numworkers * 2],
+        recv_req2[numworkers];
 
     for (dest = 1; dest <= numworkers; dest++) {
       rows = (dest <= extra) ? averow + 1 : averow;
+
       printf("Sending %d rows to task %d offset=%d\n", rows, dest, offset);
       MPI_Isend(&offset, 1, MPI_INT, dest, FROM_MASTER, MPI_COMM_WORLD,
                 &send_req[(dest - 1) * 4]);
@@ -69,26 +71,29 @@ int main(int argc, char *argv[]) {
       offset = offset + rows;
     }
 
-    MPI_Waitall(numworkers * 4, &send_req[0], &send_status[0]);
+    // MPI_Waitall(numworkers * 4, &send_req[0], &send_status[0]);
 
     /* Receive results from worker tasks */
+    int offsets_arr[numworkers];
+    int rows_arr[numworkers];
+
     for (source = 1; source <= numworkers; source++) {
-      MPI_Irecv(&offset, 1, MPI_INT, source, FROM_WORKER, MPI_COMM_WORLD,
-                &recv_req[(source - 1) * 3]);
-      MPI_Irecv(&rows, 1, MPI_INT, source, FROM_WORKER + 1, MPI_COMM_WORLD,
-                &recv_req[(source - 1) * 3 + 1]);
+      MPI_Irecv(&offsets_arr[source - 1], 1, MPI_INT, source, FROM_WORKER,
+                MPI_COMM_WORLD, &recv_req1[(source - 1) * 2]);
+      MPI_Irecv(&rows_arr[source - 1], 1, MPI_INT, source, FROM_WORKER + 1,
+                MPI_COMM_WORLD, &recv_req1[(source - 1) * 2 + 1]);
+    }
 
-      MPI_Wait(&recv_req[(source - 1) * 3], &recv_status[(source - 1) * 3]);
-      MPI_Wait(&recv_req[(source - 1) * 3 + 1],
-               &recv_status[(source - 1) * 3 + 1]);
+    MPI_Waitall(numworkers * 2, &recv_req1[0], &recv_status1[0]);
 
-      MPI_Irecv(&c[offset][0], rows * NCB, MPI_DOUBLE, source, FROM_WORKER + 2,
-                MPI_COMM_WORLD, &recv_req[(source - 1) * 3 + 2]);
-
-      MPI_Wait(&recv_req[(source - 1) * 3 + 2],
-               &recv_status[(source - 1) * 3 + 2]);
+    for (source = 1; source <= numworkers; source++) {
+      MPI_Irecv(&c[offsets_arr[source - 1]][0], rows_arr[source - 1] * NCB,
+                MPI_DOUBLE, source, FROM_WORKER + 2, MPI_COMM_WORLD,
+                &recv_req2[source - 1]);
       printf("Received results from task %d\n", source);
     }
+
+    MPI_Waitall(numworkers, &recv_req2[0], &recv_status2[0]);
 
     /* Print results */
     /*printf("****\n");
@@ -108,20 +113,26 @@ int main(int argc, char *argv[]) {
   }
   /******** worker task *****************/
   else { /* if (taskid > MASTER) */
-    MPI_Status send_status[3], recv_status[4];
+    MPI_Status recv_status[4];
     MPI_Request send_req[3], recv_req[4];
 
     MPI_Irecv(&offset, 1, MPI_INT, MASTER, FROM_MASTER, MPI_COMM_WORLD,
               &recv_req[0]);
     MPI_Irecv(&rows, 1, MPI_INT, MASTER, FROM_MASTER + 1, MPI_COMM_WORLD,
               &recv_req[1]);
+    MPI_Irecv(&b, NCA * NCB, MPI_DOUBLE, MASTER, FROM_MASTER + 3,
+              MPI_COMM_WORLD, &recv_req[3]);
+
     MPI_Wait(&recv_req[0], &recv_status[0]);
     MPI_Wait(&recv_req[1], &recv_status[1]);
 
+    MPI_Isend(&offset, 1, MPI_INT, MASTER, FROM_WORKER, MPI_COMM_WORLD,
+              &send_req[0]);
+    MPI_Isend(&rows, 1, MPI_INT, MASTER, FROM_WORKER + 1, MPI_COMM_WORLD,
+              &send_req[1]);
+
     MPI_Irecv(&a, rows * NCA, MPI_DOUBLE, MASTER, FROM_MASTER + 2,
               MPI_COMM_WORLD, &recv_req[2]);
-    MPI_Irecv(&b, NCA * NCB, MPI_DOUBLE, MASTER, FROM_MASTER + 3,
-              MPI_COMM_WORLD, &recv_req[3]);
 
     MPI_Wait(&recv_req[2], &recv_status[2]);
     MPI_Wait(&recv_req[3], &recv_status[3]);
@@ -132,17 +143,8 @@ int main(int argc, char *argv[]) {
         for (j = 0; j < NCA; j++) c[i][k] = c[i][k] + a[i][j] * b[j][k];
       }
 
-    MPI_Isend(&offset, 1, MPI_INT, MASTER, FROM_WORKER, MPI_COMM_WORLD,
-              &send_req[0]);
-    MPI_Isend(&rows, 1, MPI_INT, MASTER, FROM_WORKER + 1, MPI_COMM_WORLD,
-              &send_req[1]);
-    MPI_Wait(&send_req[0], &send_status[0]);
-    MPI_Wait(&send_req[1], &send_status[1]);
-
     MPI_Isend(&c, rows * NCB, MPI_DOUBLE, MASTER, FROM_WORKER + 2,
               MPI_COMM_WORLD, &send_req[2]);
-
-    MPI_Wait(&send_req[2], &send_status[2]);
   }
 
   MPI_Finalize();
